a318c7b9dc26b60da450c2bf748c4e1726ef3c23
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2022-05-29 14:18:18 +0700

    update configuration

--- callback_server.py
+++ callback_server.py
@@ -1,27 +1,27 @@
-from sanic import Sanic, response
-from sanic.request import Request
-from sanic.response import HTTPResponse
+# from sanic import Sanic, response
+# from sanic.request import Request
+# from sanic.response import HTTPResponse
 
 
-def create_app() -> Sanic:
+# def create_app() -> Sanic:
 
-    bot_app = Sanic(__name__, configure_logging=False)
+#     bot_app = Sanic(__name__, configure_logging=False)
 
-    @bot_app.post("/bot")
-    def print_response(request: Request) -> HTTPResponse:
-        """Print bot response to the console."""
-        bot_response = request.json.get("text")
-        print(f"\n{bot_response}")
+#     @bot_app.post("/bot")
+#     def print_response(request: Request) -> HTTPResponse:
+#         """Print bot response to the console."""
+#         bot_response = request.json.get("text")
+#         print(f"\n{bot_response}")
 
-        body = {"status": "message sent"}
-        return response.json(body, status=200)
+#         body = {"status": "message sent"}
+#         return response.json(body, status=200)
 
-    return bot_app
+#     return bot_app
 
 
-if __name__ == "__main__":
-    app = create_app()
-    port = 5034
+# if __name__ == "__main__":
+#     app = create_app()
+#     port = 5034
 
-    print(f"Starting callback server on port {port}.")
-    app.run("0.0.0.0", port)
+#     print(f"Starting callback server on port {port}.")
+#     app.run("0.0.0.0", port)
